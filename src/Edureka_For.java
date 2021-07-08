c45dfffeecc7cf2f98b2666e2c1e474ca0070255
Barbara Aghanenu <linkinbarbie@gmail.com>
2021-07-08 20:26:38 +0100

    Initial Commit

--- src/Edureka_For.java
+++ src/Edureka_For.java
@@ -1,8 +1,21 @@
+//Write a program to print 10 even numbers and 10 odd numbers
 public class Edureka_For {
     public static void main(String[] args) {
-        for(int val = 2;  val < 20 ; val++)//iterate all numbers less than 20
-            if (val % 2 == 0 ) //then identify which of these numbers can be dividied by 2
+        for(int val = 2;  val <= 21 ; val++)//iterate all numbers less than equal to 21
+            if (val % 2 == 0 ) //then identify which of these numbers can be divided by 2
             System.out.println("Divisible by 2: " + val);
+            else
+                System.out.println("Not divisible by 2: " + val);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// factorial of 3 -
+        int i = 1;
+        int n = 5;
+        //get the value to multiply the factorials by
+        for(int num = 1; num <= n; num++) //use for to get all the numbers less than equals to 3
+            i=i * num; //multiply the base number by each number incrementally
+                System.out.println(i + " is the factorial of " + n);
 
     }
 }
