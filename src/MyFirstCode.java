c21ef868a83ce16ef839280ac733535a83e99bff
Barbara Aghanenu <linkinbarbie@gmail.com>
2021-07-08 16:23:14 +0100

    Initial commit

---
+++ src/MyFirstCode.java
@@ -0,0 +1,9 @@
+public class MyFirstCode {
+
+    public static void main(String[] args) {
+        int a = 20, b = 30, c;
+        c = a + b;
+        System.out.println("Result:" + c);
+        System.out.println("Hello world, here is my first program");
+    }
+}
