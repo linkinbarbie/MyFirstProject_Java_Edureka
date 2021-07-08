ed3496e046b0a4bd2068ba4e19d0133e57ed441e
Barbara Aghanenu <linkinbarbie@gmail.com>
2021-07-08 19:47:15 +0100

    Initial Commit

---
+++ src/Edureka_For.java
@@ -0,0 +1,8 @@
+public class Edureka_For {
+    public static void main(String[] args) {
+        for(int val = 2;  val < 20 ; val++)//iterate all numbers less than 20
+            if (val % 2 == 0 ) //then identify which of these numbers can be dividied by 2
+            System.out.println("Divisible by 2: " + val);
+
+    }
+}
