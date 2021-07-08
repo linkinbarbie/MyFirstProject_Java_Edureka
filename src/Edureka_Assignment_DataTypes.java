eb26ace18472d252333d224afc9078494e2b2699
Barbara Aghanenu <linkinbarbie@gmail.com>
2021-07-08 17:34:07 +0100

    Initial Commit

---
+++ src/Edureka_Assignment_DataTypes.java
@@ -0,0 +1,53 @@
+public class Edureka_Assignment_DataTypes {
+    public static void main(String[] args) {
+        //integer
+
+        int intNumber = 200;
+        System.out.println(intNumber);
+
+        //short
+        short shtNumber = 5995;
+        System.out.println(shtNumber);
+
+        //long
+        long lngNumber = 8889899;
+        System.out.println(lngNumber);
+
+        //byte - 113
+        byte byNumber = 100;
+        System.out.println(byNumber);
+
+        //float - 7 decimal places
+        float flNumber = 7.87676767767f;//will shorten to 7 decimal places
+        System.out.println(flNumber);
+
+        //double - 15 decimal places
+        double dblNumber = 7.89999999d;
+        System.out.println(dblNumber);
+
+        //char holds characters 2 bytes
+        char chrType = 'B';
+        System.out.println(chrType);
+
+        //bool holds logical(true or false)
+        boolean bolType = true;
+        System.out.println(bolType);//can be true because the variable is assigned to true
+
+        Integer IntNumber = 23232;
+        System.out.println(IntNumber);
+
+        Float FlNumber = 45.56554554f;
+        System.out.println(FlNumber);
+
+        Character CrType = 'L';
+        System.out.println(CrType);
+
+        Boolean BlType = true;
+        System.out.println(BlType);
+
+
+
+
+
+    }
+}
